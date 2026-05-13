6c5e62b4f0d3d0a0c4d31ff93f2239dba8271699
Alejandro Madriñán Fernández <36339608+alexmadrinanfdez@users.noreply.github.com>
2026-05-13 12:24:51 -0500

    Add rules to generation

--- genpw.py
+++ genpw.py
@@ -5,4 +5,10 @@ def generate_password(length):
     if length <= 0:
         raise ValueError("Length must be positive")
     alphabet = string.ascii_letters + string.digits
-    return ''.join(secrets.choice(alphabet) for i in range(length))
+    while True:
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and any(c.isdigit() for c in password)):
+            break
+    return password
