f520b8a0ae28009a35a19b536b11fcab865bdb68
Alejandro Madriñán Fernández <36339608+alexmadrinanfdez@users.noreply.github.com>
2026-05-14 19:30:17 -0500

    Handle rules gracefully

--- genpw.py
+++ genpw.py
@@ -1,14 +1,18 @@
 import string
 import secrets
 
-def generate_password(length):
+def generate_password(length, strict=False):
     if length <= 0:
         raise ValueError("Length must be positive")
+    
     alphabet = string.ascii_letters + string.digits
-    while True:
-        password = ''.join(secrets.choice(alphabet) for i in range(length))
-        if (any(c.islower() for c in password)
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    
+    if strict:
+        # If generated password does not meet criteria, generate a new one
+        if not (any(c.islower() for c in password)
                 and any(c.isupper() for c in password)
                 and any(c.isdigit() for c in password)):
-            break
+            return generate_password(length, strict)
+    
     return password
